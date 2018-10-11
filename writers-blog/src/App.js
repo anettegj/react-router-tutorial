343beaef7a2570a7efcd754611d2c140277a029a
Anette <anette@fusetools.com>
2018-10-11 14:57:15 +0200

    Single page application with two routes
    One taking us to the Home page and one to the Writers page.

--- writers-blog/src/App.js
+++ writers-blog/src/App.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react'
-
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 
 export default class extends Component {
   state = {
     writers: []
   }
+
+  componentDidMount() {
+    fetch('http://localhost:3004/writers')
+      .then(res => res.json())
+      .then(writers => this.setState({writers}))
+  }
+
   render() {
     return (
-      <div>
-      </div>
+      <BrowserRouter>
+        <Fragment>
+          <ul>
+            <li>
+              {/** Link will help us keeping this a single page application by
+                  making sure the page doesn't reload even though we change the url */}
+              <Link to="/">Home</Link>
+            </li>
+            <li> 
+              <Link to="/writers">Writers </Link>
+            </li>
+          </ul>
+
+          {/** Without Route exact the content of Home is displayed for the Writers link 
+            as well. */}
+          <Route exact path="/" render={() => <div>Home</div>}></Route>
+          <Route path="/writers" render={ () => {return <div>Writers</div>}}/>
+        </Fragment>
+      </BrowserRouter>
     )
   }
 }
