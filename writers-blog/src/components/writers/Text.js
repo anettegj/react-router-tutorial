34d9c8e3bc14c7fde096cee2e2c365cb6f90ea21
Anette <anette@fusetools.com>
2018-10-11 23:56:17 +0200

    Fix runtime error in Text component

--- writers-blog/src/components/writers/Text.js
+++ writers-blog/src/components/writers/Text.js
@@ -3,5 +3,5 @@ import React, {Fragment} from 'react'
 export default ({title, description, published}) => 
 <Fragment>
     <h4>{title} {published?`(${published})`:''}</h4>
-    <p>{description ? desscrition : <i>No description</i>}</p>
+    <p>{description ? description : <i>No description</i>}</p>
 </Fragment>
